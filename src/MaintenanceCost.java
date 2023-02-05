e246629fd1e77e386b1b7c502cb7e2e361a62af6
Travis Feldkamp <feldkatr@mail.uc.edu>
2023-02-05 13:46:22 -0500

    Initial commit

---
+++ src/MaintenanceCost.java
@@ -0,0 +1,16 @@
+public class MaintenanceCost {
+    public static void main(String[] args) {
+      double springSeason = 682.00;
+      double summerSeason = 854.60;
+      double winterSeason = 754.80;
+      double fallSeason = 381.00;
+      double totalCost;
+      totalCost = springSeason + summerSeason + winterSeason + fallSeason;
+        System.out.println("The maintenance cost for the Spring was " + springSeason + " $,the cost of Summer was "
+                + summerSeason + "$, the cost of fall was " + fallSeason + "$, the cost of Winter was " + winterSeason +
+                "$ .The total cost was " + totalCost);
+
+
+
+    }
+}
